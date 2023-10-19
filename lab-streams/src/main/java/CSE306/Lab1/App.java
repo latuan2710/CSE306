f93bb5d28af04426fd6e5115f24a6010d48fbc53
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2023-10-19 14:12:31 +0700

    update

--- lab-streams/src/main/java/CSE306/Lab1/App.java
+++ lab-streams/src/main/java/CSE306/Lab1/App.java
@@ -4,10 +4,8 @@ package CSE306.Lab1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
