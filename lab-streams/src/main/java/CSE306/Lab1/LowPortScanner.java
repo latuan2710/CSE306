b40da216e77bf759b7b9b77575aedd181a3035dc
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2023-11-06 14:38:08 +0700

    a

---
+++ lab-streams/src/main/java/CSE306/Lab1/LowPortScanner.java
@@ -0,0 +1,23 @@
+package CSE306.Lab1;
+
+import java.io.IOException;
+import java.net.*;
+
+public class LowPortScanner {
+    public static void main(String[] args) {
+        String host = args.length > 0 ? args[0] : "localhost";
+        for (int i = 1; i < 1024; i++) {
+            try {
+                Socket socket = new Socket(host, i);
+                System.out.println("There is no server on port " + i + " of " + host);
+                socket.close();
+            } catch (UnknownHostException e) {
+                System.err.println(e);
+                break;
+            } catch (IOException e) {
+
+            }
+
+        }
+    }
+}
