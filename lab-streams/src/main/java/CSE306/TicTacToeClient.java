d20d7b5a97805ad145db4a33c723523a47aa2cf4
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2023-11-16 15:02:26 +0700

    save

---
+++ lab-streams/src/main/java/CSE306/TicTacToeClient.java
@@ -0,0 +1,53 @@
+package CSE306;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+
+public class TicTacToeClient {
+    public static void main(String[] args) {
+        BufferedReader terminal = new BufferedReader(new InputStreamReader(System.in));
+        try {
+
+            Socket socket = new Socket("localhost", 13);
+            socket.setSoTimeout(10000);
+
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+            OutputStream out = socket.getOutputStream();
+            Writer writer = new OutputStreamWriter(out, "UTF-8");
+            writer = new BufferedWriter(writer);
+
+            while (true) {
+                int count = 0;
+                String result = "";
+                while (!(result = reader.readLine()).contains("End")) {
+                    System.out.println(result);
+                    if (result.contains("]")) {
+                        count++;
+                    }
+                    if (count == 3)
+                        break;
+                }
+                String input = terminal.readLine();
+                if (input.equals("quit"))
+                    break;
+
+                writer.write(Integer.valueOf(input) + "\r\n");
+                writer.flush();
+            }
+            
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- lab-streams/src/main/java/CSE306/TicTacToeServer.java
+++ lab-streams/src/main/java/CSE306/TicTacToeServer.java
@@ -1,12 +1,92 @@
 package CSE306;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class TicTacToeServer {
     public static void main(String[] args) {
-        int mat[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        String[][] matrix = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+        List<Integer> numbers = new ArrayList<>();
 
-        for (int[] row : mat)
-            System.out.println(Arrays.toString(row));
+        for (int i = 1; i <= 9; i++) {
+            numbers.add(i);
+        }
+
+        try (ServerSocket server = new ServerSocket(13)) {
+            Socket connection = server.accept();
+
+            Writer out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+            out = new BufferedWriter(out);
+
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+            out.write(display(matrix) + "\r\n");
+            out.flush();
+
+            String readLine;
+            while (!(readLine = reader.readLine()).equals("quit") && !numbers.isEmpty()) {
+                int client_input = Integer.valueOf(readLine);
+                numbers.remove(Integer.valueOf(client_input));
+
+                if (numbers.isEmpty()) {
+                    matrix = tickMatrix(matrix, client_input, "x");
+                    out.write(display(matrix) + "\r\n");
+                    out.flush();
+                    break;
+                }
+
+                int random_number = getRandomElement(numbers);
+
+                matrix = tickMatrix(matrix, client_input, "x");
+                matrix = tickMatrix(matrix, random_number, "o");
+
+                numbers.remove(Integer.valueOf(random_number));
+                System.out.println(numbers);
+                out.write(display(matrix) + "\r\n");
+                out.flush();
+            }
+
+            connection.close();
+            server.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    static String display(String[][] matrix) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String[] row : matrix)
+            stringBuilder.append(Arrays.toString(row) + "\n");
+        return stringBuilder.toString();
+    }
+
+    static String[][] tickMatrix(String[][] matrix, int index, String tick) {
+        if (index >= 1 && index <= 3)
+            matrix[0][index - 1] = tick;
+        else if (index >= 4 && index <= 6)
+            matrix[1][index - 4] = tick;
+        else if (index >= 7 && index <= 9)
+            matrix[2][index - 7] = tick;
+
+        return matrix;
+    }
+
+    static int getRandomElement(List<Integer> list) {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
     }
 }
